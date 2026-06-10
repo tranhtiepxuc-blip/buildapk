a7937daf88363e301c9c454f19816c7c93bf49d0
tranhtiepxuc-blip <tranhtiepxuc@gmail.com>
2026-06-10 10:27:47 +0700

    Create IXposedHookLoadPackage.java

---
+++ src/de/robv/android/xposed/IXposedHookLoadPackage.java
@@ -0,0 +1,6 @@
+package de.robv.android.xposed;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public interface IXposedHookLoadPackage {
+    void handleLoadPackage(LoadPackageParam lpparam) throws Throwable;
+}
