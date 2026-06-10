0b6f44620775a86da25eeed8d141352dc62ba2a8
tranhtiepxuc-blip <tranhtiepxuc@gmail.com>
2026-06-10 10:29:05 +0700

    Create XposedHelpers.java

---
+++ src/de/robv/android/xposed/XposedHelpers.java
@@ -0,0 +1,5 @@
+package de.robv.android.xposed;
+
+public class XposedHelpers {
+    public static void findAndHookMethod(Class<?> clazz, String methodName, Object... parameterTypesAndCallback) {}
+}
